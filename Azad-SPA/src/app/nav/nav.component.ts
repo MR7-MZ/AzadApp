5d79c24192a0346baab5f6d3b04b80522fd49df5
MR7 <55983408+MR7DarkKnight@users.noreply.github.com>
2020-08-04 05:03:06 -0700

    End of Section 6

--- Azad-SPA/src/app/nav/nav.component.ts
+++ Azad-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authSevice: AuthService) { }
+  constructor(public authSevice: AuthService , private alertify: AlertifyService) { }
 
   // tslint:disable-next-line:typedef
   ngOnInit() {
@@ -18,22 +19,21 @@ export class NavComponent implements OnInit {
   // tslint:disable-next-line:typedef
   login(){
     this.authSevice.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+     this.alertify.success('Logged in successfully');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   // tslint:disable-next-line:typedef
   logIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+   return this.authSevice.loggedIn();
   }
 
   // tslint:disable-next-line:typedef
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
